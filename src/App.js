340951c5c984edc9fc55cfe1d53876009a57477d
metinozkan <metinozkan@metinozkan-air.local>
2022-11-27 18:28:39 +0300

    shoe landing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { ContactShadows, useGLTF, OrbitControls } from "@react-three/drei";
+import { Mercedes } from "./Mercedes";
+import Underlay from "./Underlay";
+import { Shoe } from "./Shoe";
 
-function App() {
+useGLTF.preload("/shoe-draco.glb");
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Underlay />
+
+      <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 1.1], fov: 50 }}>
+        <ambientLight intensity={2} />
+        <spotLight
+          position={[1, 6, 1.5]}
+          angle={0.2}
+          penumbra={1}
+          intensity={2.5}
+          castShadow
+          shadow-mapSize={[2048, 2048]}
+        />
+        <spotLight
+          position={[-5, 5, -1.5]}
+          angle={0.03}
+          penumbra={1}
+          intensity={4}
+          castShadow
+          shadow-mapSize={[1024, 1024]}
+        />
+        <spotLight
+          position={[5, 5, -5]}
+          angle={0.3}
+          penumbra={1}
+          intensity={4}
+          castShadow={true}
+          shadow-mapSize={[256, 256]}
+          color="#ffffc0"
+        />
+        <Suspense fallback={null}>
+          {/* <Shoe scale={0.225} position={[0, -0.09, 0]} /> */}
+
+          {/* <Mercedes /> */}
+          <Shoe scale={0.225} position={[0, -0.09, 0]} />
+          <ContactShadows
+            frames={1}
+            rotation-x={[Math.PI / 2]}
+            position={[0, -0.33, 0]}
+            far={0.4}
+            width={2}
+            height={2}
+            blur={4}
+          />
+        </Suspense>
+        <OrbitControls
+          enablePan={false}
+          enableZoom={false}
+          minPolarAngle={Math.PI / 2.2}
+          maxPolarAngle={Math.PI / 2.2}
+        />
+      </Canvas>
+    </>
   );
 }
-
-export default App;
